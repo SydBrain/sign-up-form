7cf2964198b64732cb7858e0c09fa8d124c1a711
SydBrain <103507827+SydBrain@users.noreply.github.com>
2023-10-26 19:18:55 +0200

    Added show/hide icons to password fields

--- js/app.js
+++ js/app.js
@@ -1,3 +1,21 @@
- document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", () => {
 
-})
+    // DOM Elements
+    const passwordInputs = document.querySelectorAll('[data-password]');
+    const togglePasswordIcons = document.querySelectorAll('.toggle-password-icon');
+
+    // Event Listeners
+    togglePasswordIcons.forEach(passwordIcon => {
+        passwordIcon.addEventListener('click', togglePasswordVisibility);
+    });
+
+    // Functions
+    function togglePasswordVisibility(event) {
+        const passwordIcon = event.target;
+        const passwordInput = passwordIcon.closest('.password-input').querySelector('[data-password]');
+        const inputType = passwordInput.getAttribute("type") === "password" ? "text" : "password";
+        passwordInput.setAttribute("type", inputType);
+        const iconPath = inputType === "password" ? "/assets/images/eye-off.svg" : "/assets/images/eye.svg";
+        passwordIcon.setAttribute("src", iconPath);
+    }
+});
